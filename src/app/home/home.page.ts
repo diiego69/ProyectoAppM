0e14ba430b59135ed82b501360b354489e6ccfe5
Diiego <gonzalez.nunez8@educagrafica.cl>
2024-10-20 18:08:07 -0300

    se limpian archivos y codigos que no se utilizaban, ademas se elimina todo lo relacionado a android (estaba mal configurado)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { MenuController } from '@ionic/angular';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -37,22 +36,5 @@ export class HomePage {
 
   goToProfile() {
     this.router.navigate(['/profile']);
-  }
-
-  async openCamera() {
-    try {
-      const image = await Camera.getPhoto({
-        quality: 90,
-        allowEditing: false,
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera 
-      });
-
-      this.capturedImage = image.webPath;
-      console.log('Imagen capturada:', this.capturedImage);
-
-    } catch (error) {
-      console.error('Error al capturar imagen', error);
-    }
   }
 }
