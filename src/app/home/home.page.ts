0022bf14d00867918adbce2e9c20fdc18feb2acb
diiego69 <141367556+diiego69@users.noreply.github.com>
2024-10-24 17:27:07 -0300

    Se agregan funcionalidades de la camara, por lo que se pueden leer codigos qrs y tomar la ubicacion de este

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { MenuController } from '@ionic/angular';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint, CapacitorBarcodeScannerTypeHintALLOption } from '@capacitor/barcode-scanner';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -36,5 +38,32 @@ export class HomePage {
 
   goToProfile() {
     this.router.navigate(['/profile']);
+  }
+  result: string = ''
+
+  async scan(): Promise<void> {
+    const result = await CapacitorBarcodeScanner.scanBarcode({
+      hint: CapacitorBarcodeScannerTypeHint.ALL
+    });
+    this.result = result.ScanResult
+    this.getLocation();
+    console.log(result.ScanResult);
+  }
+
+  async getLocation() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      const latitud = position.coords.latitude;
+      const longitud = position.coords.longitude;
+      console.log(`Latitud: ${latitud}, Longitud: ${longitud}`);
+  
+      if (latitud >= -33.468 && latitud <= -33.467 && longitud >= -70.635 && longitud <= -70.634) {
+        console.log('tabien.');
+      } else {
+        console.log('tamal.');
+      }
+    } catch (error) {
+      console.error('No se pudo encontrar ubicacion:', error);
+    }
   }
 }
