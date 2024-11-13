aeda6726200e97bf4ca55b1a1e232b2dd4d6c435
diiego69 <141367556+diiego69@users.noreply.github.com>
2024-11-13 05:49:56 -0300

    Se cambian versiones de los plugins para que no hayan errores, se arregla camara (cambiando codigo) y ahora se puede crear apk y abrir camara para escanear qrs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'ionic-app-base',
-  webDir: 'www'
+  appId: 'io.ionic.starter',
+  appName: 'ProyectoAppM',
+  webDir: 'www',
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
